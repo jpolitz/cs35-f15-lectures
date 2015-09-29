b7d52d2c98c40465419bcf89be22dc7a06132048
Joe Gibbs Politz <joe.politz@gmail.com>
2015-09-29 10:13:45 -0400

    starter for lecture 9

---
+++ lecture-9/arrayStringList.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include "stringList.h"
+#include "arrayStringList.h"
+
+using namespace std;
+
+ASList::ASList() {
+  size = 0;
+  capacity = 2;
+  elements = new string[capacity];
+}
+
+ASList::~ASList() {
+  delete [] elements;
+}
+
+int ASList::getSize() {
+  return size;
+}
+
+bool ASList::isEmpty() {
+  return (size == 0);
+}
+
+void ASList::expandCapacity() {
+  if(size == capacity) {
+    string* newArrayPtr = new string[capacity + 1];
+    capacity += 1;
+    for(int x = 0; x < size; x += 1) {
+      newArrayPtr[x] = elements[x];
+    }
+    delete [] elements;
+    elements = newArrayPtr;
+  }
+}
+
+void ASList::addTail(string s) {
+  expandCapacity();
+  elements[size] = s;
+  size += 1;
+}
+
+void ASList::addHead(string s) {
+  expandCapacity();
+  for(int i = size; i > 0; i -= 1) {
+    elements[i] = elements[i - 1];
+  }
+  elements[0] = s;
+  size += 1;
+}
+
+
+void ASList::set(int index, string s) {
+  if(index >= size) {
+    throw runtime_error("Index out of range");
+  }
+  elements[index] = s;
+}
+
+string ASList::get(int index) {
+  if(index >= size) {
+    throw runtime_error("Index out of range");
+  }
+  return elements[index];
+}
+
+
---
+++ lecture-9/arrayStringList.h
@@ -0,0 +1,26 @@
+#include <string>
+#include "stringList.h"
+
+using namespace std;
+
+class ASList : public SList {
+  private:
+    int size;
+    // How many elements currently stored
+    int capacity;
+    // How many elements could fit before making a bigger array
+    string* elements;
+
+    void expandCapacity();
+  public:
+    ASList();
+    ~ASList();
+    bool isEmpty();
+    int getSize();
+    void addHead(string s);
+    void addTail(string s);
+    void set(int index, string s);
+    string get(int index);
+};
+
+
